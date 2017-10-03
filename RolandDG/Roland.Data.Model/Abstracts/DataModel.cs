984474e5d75edfa94bb8c0053109221f6be52c6e
purlantov <ppurlantov@yahoo.com>
2017-10-03 11:28:43 +0300

    init data

---
+++ RolandDG/Roland.Data.Model/Abstracts/DataModel.cs
@@ -0,0 +1,21 @@
+﻿using Roland.Data.Model.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roland.Data.Model.Abstracts
+{
+    public abstract class DataModel : IAuditable, IDeletable
+    {
+        public DateTime? CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+    }
+}
+ 
